dd92d727b1d98c1126018e4c2df564acb427e196
Andrew Schwartz <ozydingo@gmail.com>
2019-10-19 16:47:36 -0400

    Initial import and reorganization of GCP-hosted function code, with README

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # pluralzbot
 Obnoxiously ask to change your slack messages for you
+
+## What is doez
+
+Pluralzbot will watch your messages in installed channels for any messages that end in a plural word spelled with an 's'. It will then ~obnoxiously~ kindly suggest you change this spelling to end in a 'z'.
+
+Pluralzbot also has the ability to automatically change your messages for you for super convenience. However, this feature is on hold until multi-user token storage is worked out.
+
+## How it doez
+
+1. Create a Google Cloud function using the Node.js 8 runtime.
+2. Copy the files in `src` into this function.
+3. Set the "Function To Execute" to `verify`.
+4. Paste the Google Cloud function http trigger into the Slack app. Verification should succeed.
+5. Modify the "Function To Execute" to `main`.
+6. Edit the function, scroll down to environment variables. Add the following variables:
+  * `BOT_TOKEN`: copy this from the Slack app.
+  * `TEST_CHANNEL`: get the channel id that is being monitored (this will soon be removed). Should look like `GABCD1234` (private channel) or `CABCD1234` (public channel)
+7. Deploy the function and start typing in the slack channel!
+
+## Notes
+
+This is what a message post event looks like to the function:
+
+```
+"{ token: 'abc123',
+  team_id: 'TABC123',
+  api_app_id: 'AABC123',
+  event:
+   { client_msg_id: 'uuid-numbers-and-letters',
+     type: 'message',
+     text: 'hello, world',
+     user: 'UABC123',
+     ts: '1234567890.000000',
+     team: 'T0JRWG6CC',
+     channel: 'GABC123',
+     event_ts: '1234567890.000000',
+     channel_type: 'group' },
+  type: 'event_callback',
+  event_id: 'EABC123',
+  event_time: 1234567890,
+  authed_users: [ 'UABC123' ] }"
+```
---
+++ src/index.js
@@ -0,0 +1,109 @@
+const BOT_TOKEN = process.env["BOT_TOKEN"];
+const channel = process.env["TEST_CHANNEL"];
+
+const axios = require('axios');
+
+const botHeaders = {
+  "Authorization": `Bearer ${BOT_TOKEN}`,
+  "Content-type": "application/json; charset=utf-8",
+};
+
+// Respond to initial challenge when adding URL to the Slack app.
+// This function is not needed thereafter.
+exports.verify = (req, res) => {
+  const body = req.body;
+  if (body.challenge) {
+    res.status(200).send(body.challenge);
+  }
+}
+
+// Main event function handler
+exports.main = async (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  // event fields: { client_msg_id, user, ts, text, channel }
+  const event = body.event || {};
+  const { text } = event;
+
+  if (eventInScope(event) && hasPlural(text)) {
+    await handlePluralz(event);
+  }
+
+  res.status(200).send('');
+};
+
+function eventInScope(event) {
+  const { text, channel, type, subtype } = event;
+
+  if (channel !== channel) { return false; }
+  if (subtype === "message_changed" ) { return false; }
+  if (!text) { return false; }
+
+  return true
+}
+
+function getUserToken(user) {
+  // TODO: Look up from db store based on `user`
+  return null;
+}
+
+function handlePluralz(event) {
+  const { user } = event;
+  const token = getUserToken(user);
+  if (token) {
+    postPluralz(event, token);
+  } else {
+    suggestPluralz(event);
+  }
+}
+
+function hasPlural(text) {
+  // Rough cut: ends in s following a non-s consonant,
+  // and is at least four letters long
+  return (
+    text.length >= 4 &&
+    /\w+s$/.test(text) &&
+    !/[aeious]/.test(text[text.length-2])
+  )
+}
+
+function correctText(text) {
+  return text.replace(/\b(\w+)s$/g, "$1z");
+}
+
+function userHeaders(token) {
+  return {
+    "Authorization": `Bearer ${token}`,
+    "Content-type": "application/json; charset=utf-8"
+  };
+}
+
+function suggestPluralz({ channel }) {
+  // TODO: use postEphemeral with interactive component the install app
+  // Or, not ephemeral because that's more fun
+  axios({
+    method: 'POST',
+    url: 'https://slack.com/api/chat.postMessage',
+    headers: botHeaders,
+    data: {
+      text: 'Hi therez! It lookz like you may have made some errorz in spelling plural words. Would you like to correct your mistakez by using "z" for pluralz?',
+      channel: channel
+    },
+  }).then(response => console.log("Response for suggestion:", response.data));
+}
+
+function postPluralz({ ts, text, channel }, token) {
+  const data = {
+    text: correctText(text),
+    ts: ts,
+    channel: channel,
+    as_user: true,
+  };
+  axios({
+    method: 'POST',
+    url: 'https://slack.com/api/chat.update',
+    headers: userHeaders(token),
+    data: data,
+  }).then(response => console.log("Response for correction:", response.data));
+}
