cef26f490a3b0064d9fb80b23bf147f4efe12dbe
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-10-25 17:22:44 -0400

    Improve plural detection

--- src/pluralz.js
+++ src/pluralz.js
@@ -1,13 +1,25 @@
+// This is a funuction becase `exec` can only be rnu once?
+function pattern() {
+  return /\b(\w+)s([.?!]*)$/g;
+}
+
 exports.replace = (text) => {
-  return text.replace(/\b(\w+)s$/g, "$1z");
+  return text.replace(pattern(), "$1z$2");
 }
 
 exports.hasPlural = (text) => {
   // Rough cut: ends in s following a non-s consonant,
   // and is at least four letters long
-  return (
-    text.length >= 4 &&
-    /\w+s$/.test(text) &&
-    !/[aeious]/.test(text[text.length-2])
-  )
+  if (text.length < 4) { return false; }
+  if (/`/.test(text)) { return false; }
+
+  const result = pattern().exec(text);
+  if (!result) { return false; }
+
+  const [, ...matches] = result;
+  return matches.slice(0, -1).filter(match => (
+    match.length > 0
+  )).map(match => (
+    !/[aeious]/.test(match[match.length-1])
+  ))
 }
