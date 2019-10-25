da56910119684b8029e2a9bae2a6cf8551668977
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-10-25 11:26:09 -0400

    Fix spelling: words -> wordz

--- src/pluralz.js
+++ src/pluralz.js
@@ -2,7 +2,7 @@ exports.replace = (text) => {
   return text.replace(/\b(\w+)s$/g, "$1z");
 }
 
-exports.suggestion = 'Hi therez! It lookz like you may have made some errorz in spelling plural words. Would you like to correct your mistakez by using "z" for pluralz?';
+exports.suggestion = 'Hi therez! It lookz like you may have made some errorz in spelling plural wordz. Would you like to correct your mistakez by using "z" for pluralz?';
 
 exports.hasPlural = (text) => {
   // Rough cut: ends in s following a non-s consonant,
