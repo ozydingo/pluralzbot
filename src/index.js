07b4d4dfeed528451a7b04158f56a3d4d864c5c1
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-10-25 09:53:22 -0400

    Update bugged_at field when suggesting pluralz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const BUG_TIME_THRESH = 1 * 60 * 1000;
 const test_channel = process.env["TEST_CHANNEL"];
 
 const axios = require('axios');
@@ -37,22 +38,34 @@ function eventInScope(event) {
   return true
 }
 
+function timeToBugAgain(buggedAt) {
+  return (new Date() - buggedAt) > BUG_TIME_THRESH;
+}
+
 async function handleMessage(event) {
   const { ts, text, channel, user: userId } = event;
   if (!pluralz.hasPlural(text)) { return; }
 
-  const user = await users.find(userId);
-  if (user && user.participation === 'ignore') {
+  const user = await users.find_or_create(userId);
+  const userData = user.data();
+  if (userData.participation === 'ignore') {
+    console.log(`Action: ignore user ${userId}.`)
     return;
-  } else if (user && user.token) {
-    correctPluralz({ ts, text, channel, token: user.token });
-  } else {
+  } else if (userData.participation === 'autocorrect' && userData.token) {
+    console.log(`Action: correct user ${userId}.`)
+    correctPluralz({ ts, text, channel, token: userData.token });
+  } else if (!user.participation || timeToBugAgain(userData.bugged_at)) {
+    console.log(`Action: time to bug user ${userId}!`)
     suggestPluralz({ userId, channel });
+  } else {
+    console.log(`Action: we're hiding from user ${userId}.`)
+    return;
   }
 }
 
 function suggestPluralz({ userId, channel }) {
   axios(slackz.suggestion({ userId, channel })).then(response => {
+    users.touch(userId);
     console.log("Response for suggestion:", response.data);
   });
 }
