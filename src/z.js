50e1c99ca32aaceffc1db1ab6b9d83c21238a9e3
Andrew Schwartz <ozydingo@gmail.com>
2019-12-27 16:40:59 -0500

    Use const or let over var

--- src/z.js
+++ src/z.js
@@ -1,26 +1,26 @@
-var pos = require("ni-pos");
-var pluralize = require("pluralize");
+const pos = require("ni-pos");
+const pluralize = require("pluralize");
 
 const ACCEPTED_TAG_TYPE = ["NNS", "NNPS"];
 const SYMBOL_TAG_TYPE = [",", ".", ":", "$", "#", '"', ")", "("];
 
 function isAnEmoji(currentIndex, taggedWords) {
   return (
-    taggedWords[currentIndex + 2] && 
+    taggedWords[currentIndex + 2] &&
     taggedWords[currentIndex + 2][0] === ":"
   );
 }
 
 function replacez(sentence){
-  var sentenceWordz = sentence.split(" ");
-  var words = new pos.Lexer().lex(sentence);
-  var wordz = [];
-  var tagger = new pos.Tagger();
-  var taggedWords = tagger.tag(words);
-  for (var i = 0; i < taggedWords.length; i++) {
-    var taggedWord = taggedWords[i];
-    var word = taggedWord[0];
-    var tag = taggedWord[1];
+  const sentenceWordz = sentence.split(" ");
+  const words = new pos.Lexer().lex(sentence);
+  const wordz = [];
+  const tagger = new pos.Tagger();
+  const taggedWords = tagger.tag(words);
+  for (let ii = 0; ii < taggedWords.length; ii++) {
+    const taggedWord = taggedWords[ii];
+    let word = taggedWord[0];
+    const tag = taggedWord[1];
 
     if (ACCEPTED_TAG_TYPE.includes(tag) && pluralize.isPlural(word)) {
       word = pluralize.singular(word) + "z";
@@ -29,9 +29,9 @@ function replacez(sentence){
     if (SYMBOL_TAG_TYPE.includes(tag)) {
       // Combine current word with the next 2, if emoji
       // Slack emoji format example :partyparrot:
-      if (word === ":" && isAnEmoji(Number(i), taggedWords)) {
-        word = word + taggedWords[i + 1][0] + ":";
-        i += 2;
+      if (word === ":" && isAnEmoji(Number(ii), taggedWords)) {
+        word = word + taggedWords[ii + 1][0] + ":";
+        ii += 2;
       }
 
       // Lexer treats symbols as a type. To check if the symbol had a space between itself and the previous word
@@ -39,7 +39,7 @@ function replacez(sentence){
       // 2. if so, push the combined word to the new word array
       // 3. if not, push the previous word, followed by the symbol to the new array
       previousWord = wordz.pop();
-      wordWithSymbol = previousWord + word;     
+      wordWithSymbol = previousWord + word;
       if (sentenceWordz.includes(wordWithSymbol)) {
         word = wordWithSymbol;
       } else {
